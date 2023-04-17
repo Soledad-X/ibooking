5cf49f14ba41d381dc5ab48feb6cc34a060799bf
Penglei Mao <Penglei.X.Mao@gmail.com>
2023-04-17 08:36:33 +0000

    Spring web program init

---
+++ src/main/java/com/spm/ibooking/IbookingApplication.java
@@ -0,0 +1,13 @@
+package com.spm.ibooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spm/ibooking/IbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spm.ibooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
