f79ef445b93ae6192c5b6364a711ee9f5b98915d
Penglei Mao <Penglei.X.Mao@gmail.com>
2023-04-22 17:44:41 +0800

    ..

--- src/main/java/com/spm/ibooking/services/CampusService.java
+++ src/main/java/com/spm/ibooking/services/CampusService.java
@@ -3,7 +3,6 @@ package com.spm.ibooking.services;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.*;
 
 import com.spm.ibooking.exceptions.ResourceNotFoundException;
