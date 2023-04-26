600815482f998572fe8daa0c0d1d7ac894a1ce3d
Penglei Mao <Penglei.X.Mao@gmail.com>
2023-04-26 14:59:11 +0800

    Enable CORS

---
+++ src/main/java/com/spm/ibooking/configs/WebConfig.java
@@ -0,0 +1,19 @@
+package com.spm.ibooking.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
