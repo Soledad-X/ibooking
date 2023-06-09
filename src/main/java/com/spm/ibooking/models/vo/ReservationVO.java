49634628fb9d505d87eed124684ce150c6c37824
Penglei Mao <Penglei.X.Mao@gmail.com>
2023-06-09 21:49:03 +0800

    fixed

--- src/main/java/com/spm/ibooking/models/vo/ReservationVO.java
+++ src/main/java/com/spm/ibooking/models/vo/ReservationVO.java
@@ -3,9 +3,6 @@ package com.spm.ibooking.models.vo;
 import java.io.Serializable;
 import java.util.Date;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.spm.ibooking.models.enums.ReservationStatus;
 
@@ -30,12 +27,8 @@ public class ReservationVO implements Serializable {
 
     private SeatVO seat;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startTime;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date endTime;
 
     private ReservationStatus status;
